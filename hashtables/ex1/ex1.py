4cfc956c49366ca0ab762d93841fd0b1507b1129
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-08-14 22:02:15 -0400

    working on ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,10 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
+    d = {}
+    # create a dict, where the list of weights will be the key
+    # and the value of the key would be the index
+    for i in range(0, len(weights)):
+        d[weights] = i
+
     return None
