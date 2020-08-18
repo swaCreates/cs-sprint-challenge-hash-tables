afb28fc3699efaeea7933e0293540b0f1e86ca43
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-08-18 18:21:26 -0400

    completed problem 3

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -5,10 +5,20 @@ def get_indices_of_item_weights(weights, length, limit):
     # Your code here
 
     my_dict = {}
+    
     # create a dict, where the list of weights will be the key
         #dict[key(weights)] = weights[i]
     # and the value of the key would be the index
-    for i in weights:
-        my_dict[i] = weights[i]
 
+    # go through the list
+    # check if the limit - index has the result in the list
+    # if not go to next index
+    for i, weight in enumerate(weights):
+        # print(i, weight)
+        if weight not in my_dict:
+            my_dict[weight] = i
+        if limit - my_dict[weight] == limit:
+            return my_dict[weights[i]]
+        else:
+            i += 1
     return None
--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,9 +3,29 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    my_dict = dict()
+    result = []
 
+    # for the lists in the array:
+    for lists in arrays:
+        # we want to loop through the inner lists
+        for nums in lists:
+            # see/find all elements that are in each list:
+            if nums not in my_dict:
+                # add to dict
+                my_dict[nums] = 1
+            # else: if it is already there:
+            else:
+                # add +1 since it was in another list
+                my_dict[nums] += 1
+        
+    # then from here we want to check if we have seen a vals:
+        # and append any reoccurences in the result arr
+        
+    for num, count in my_dict.items():
+        if count > 1:
+            result.append(num)
     return result
-
 
 if __name__ == "__main__":
     arrays = []
