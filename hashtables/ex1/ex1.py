23fda58106b2619272c8aec46d4eb6fc3f3b9589
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-08-18 20:07:24 -0400

    working on problem one

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -12,13 +12,21 @@ def get_indices_of_item_weights(weights, length, limit):
 
     # go through the list
     # check if the limit - index has the result in the list
+    # if it does return values that sum to the limit
     # if not go to next index
+    
     for i, weight in enumerate(weights):
         # print(i, weight)
         if weight not in my_dict:
             my_dict[weight] = i
-        if limit - my_dict[weight] == limit:
+
+    for i, weight in enumerate(weights):
+        weight2 = limit - weight
+
+        if limit - my_dict[weight] == weight2:
             return my_dict[weights[i]]
-        else:
-            i += 1
+    # if limit - my_dict[weight] == weight2:
+    #     return my_dict[weights[i]]
+    # else:
+    #     my_dict[weights[i]] += 1
     return None
